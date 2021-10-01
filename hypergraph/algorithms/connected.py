2e96cad02be34dc5d10e74b58ded82f2fc969b1c
nwlandry <nicholas.landry.91@gmail.com>
2021-10-01 10:18:31 -0600

    Fixed connected module and added examples in the tutorial 1 notebook

--- hypergraph/algorithms/connected.py
+++ hypergraph/algorithms/connected.py
@@ -5,14 +5,17 @@ import scipy.sparse as sparse
 
 
 def is_connected(H, s=1):
-    A = sparse.find(hg.clique_motif_matrix(H) >= s)
-    print(A)
-    return nx.is_connected(nx.Graph(zip(A.row, A.col)))
+    data = sparse.find(hg.clique_motif_matrix(H) >= s)
+    rows = data[0]
+    cols = data[1]
+    return nx.is_connected(nx.Graph(zip(rows, cols)))
 
 
 def connected_components(H, s=1):
-    A = sparse.find(hg.clique_motif_matrix(H) >= s)
-    return nx.connected_components(nx.Graph(A))
+    data = sparse.find(hg.clique_motif_matrix(H) >= s)
+    rows = data[0]
+    cols = data[1]
+    return nx.connected_components(nx.Graph(zip(rows, cols)))
 
 
 def number_connected_components(H, s=1):
@@ -20,5 +23,7 @@ def number_connected_components(H, s=1):
 
 
 def node_connected_component(H, n, s=1):
-    A = sparse.find(hg.clique_motif_matrix(H) >= s)
-    return nx.node_connected_component(nx.Graph(A), n)
+    data = sparse.find(hg.clique_motif_matrix(H) >= s)
+    rows = data[0]
+    cols = data[1]
+    return nx.node_connected_component(nx.Graph(zip(rows, cols)), n)
