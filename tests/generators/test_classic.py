605b09b98a7ffc57ea80f374115b7c161eb01cbe
maximelucas <ml.maximelucas@gmail.com>
2022-02-14 14:36:38 +0100

    feat: added star_clique generator and corresponding tests

--- tests/generators/test_classic.py
+++ tests/generators/test_classic.py
@@ -1,6 +1,21 @@
+import pytest
 import xgi
 
 
 def test_empty_hypergraph():
     H = xgi.empty_hypergraph()
     assert H.shape == (0, 0)
+
+def test_star_clique(): 
+
+    with pytest.raises(ValueError):
+        H = xgi.erdos_renyi_hypergraph(-1, 7, 3)
+    with pytest.raises(ValueError):
+        H = xgi.erdos_renyi_hypergraph(6, -1, 3)
+    with pytest.raises(ValueError):
+        H = xgi.erdos_renyi_hypergraph(6, 7, -1)
+    with pytest.raises(ValueError):
+        H = xgi.erdos_renyi_hypergraph(6, 7, 7)
+
+    H = xgi.erdos_renyi_hypergraph(6, 7, 3)
+    assert H.number_of_nodes() == 13
--- xgi/generators/classic.py
+++ xgi/generators/classic.py
@@ -2,11 +2,11 @@
 in this module return a Hypergraph class (i.e. a simple, undirected hypergraph).
 """
 
+from itertools import combinations
+
 from xgi.classes import Hypergraph
 
-__all__ = [
-    "empty_hypergraph",
-]
+__all__ = ["empty_hypergraph", "star_clique"]
 
 
 def empty_hypergraph(create_using=None, default=Hypergraph):
@@ -44,4 +44,63 @@ def empty_hypergraph(create_using=None, default=Hypergraph):
     else:
         # try create_using as constructor
         H = create_using()
+    return H
+
+
+def star_clique(n_star, n_clique, d_max):
+    """Generate a star-clique structure
+
+    That is a star network and a clique network,
+    connected by one pairwise edge. In the clique
+    network, the each clique is promoted to a hyperedge
+    up to order d_max.
+
+    Parameters
+    ----------
+    n_star : int
+        Number of legs of the star
+    n_clique : int
+        Number of nodes in the clique
+    d_max : int
+        Maximum order up to which to promote
+        cliques to hyperedges
+
+    Returns
+    -------
+    H : xgi.Hypergraph
+
+    Notes
+    -----
+    The total number of nodes is n_star + n_clique.
+
+    """
+
+    if n_star <= 0:
+        raise ValueError("n_star must be an integer > 0.")
+    if n_clique <= 0:
+        raise ValueError("n_clique must be an integer > 0.")
+    if d_max < 0:
+        raise ValueError("d_max must be an integer >= 0.")
+    elif d_max > n_clique - 1:
+        raise ValueError("d_max must be <= n_clique - 1.")
+
+    nodes_star = range(n_star)
+    nodes_clique = range(n_star, n_star + n_clique)
+    nodes = list(nodes_star) + list(nodes_clique)
+
+    H = empty_hypergraph()
+    H.add_nodes_from(nodes)
+
+    # add star edges (center of the star is 0-th node)
+    for i in range(1, n_star):
+        H.add_edge((nodes_star[0], nodes_star[i]))
+
+    # connect clique and star by adding last star leg
+    H.add_edge((nodes_star[0], nodes_clique[0]))
+
+    # add clique hyperedges up to order d_max
+    for d in range(1, d_max):
+        for hyperedge in combinations(nodes_clique, d + 1):
+            H.add_edge(hyperedge)
+
     return H
