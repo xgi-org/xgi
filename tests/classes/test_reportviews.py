27811a1f9580b52b791a77cafd5d0f931e8c0d6b
leotrs <leo@leotrs.com>
2022-03-23 16:23:47 +0100

    add tests

---
+++ tests/classes/test_reportviews.py
@@ -0,0 +1,58 @@
+import pytest
+
+import xgi
+from xgi.exception import XGIError
+
+
+def test_edge_order(edgelist3):
+    H = xgi.Hypergraph(edgelist3)
+
+    with pytest.raises(TypeError):
+        H.edges()
+
+    assert len(H.edges(order=1)) == 1
+    assert len(H.edges(order=3)) == 0
+
+    ord2 = H.edges(order=2)
+    assert len(ord2) == 2
+    assert (0 in ord2) and (2 in ord2)
+
+    H.add_edge([3, 7, 8, 9, 10])
+
+    assert len(H.edges(order=4)) == 1
+    assert 3 in H.edges(order=4)
+
+    H.add_node_to_edge(0, 7)
+
+    assert 0 not in H.edges(order=2)
+    assert len(H.edges(order=2)) == 1
+    assert 2 in H.edges(order=2)
+
+
+def test_node_degree(edgelist3):
+    H = xgi.Hypergraph(edgelist3)
+
+    with pytest.raises(TypeError):
+        H.edges()
+
+    assert len(H.nodes(degree=1)) == 4
+    assert len(H.nodes(degree=3)) == 0
+    deg2 = H.nodes(degree=2)
+    assert len(deg2) == 2
+    assert (3 in deg2) and (4 in deg2)
+
+    H.add_edge([3, 7])
+
+    assert len(H.nodes(degree=2)) == 1
+    assert len(H.nodes(degree=3)) == 1
+    assert 3 in H.nodes(degree=3)
+    assert 7 in H.nodes(degree=1)
+
+    H.add_node_to_edge(0, 7)
+    assert 7 in H.nodes(degree=2)
+
+
+def test_edge_members(edgelist3):
+    H = xgi.Hypergraph(edgelist3)
+    assert H.edges.members() == [[1, 2, 3], [3, 4], [4, 5, 6]]
+    assert H.edges.members(dtype=dict) == {0: [1, 2, 3], 1: [3, 4], 2: [4, 5, 6]}
