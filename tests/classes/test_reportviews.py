ce17bf3a0685e6f3aa43dee467332147637e3fc7
leotrs <leo@leotrs.com>
2022-03-23 16:24:48 +0100

    remove wrong test

--- tests/classes/test_reportviews.py
+++ tests/classes/test_reportviews.py
@@ -50,9 +50,3 @@ def test_node_degree(edgelist3):
 
     H.add_node_to_edge(0, 7)
     assert 7 in H.nodes(degree=2)
-
-
-def test_edge_members(edgelist3):
-    H = xgi.Hypergraph(edgelist3)
-    assert H.edges.members() == [[1, 2, 3], [3, 4], [4, 5, 6]]
-    assert H.edges.members(dtype=dict) == {0: [1, 2, 3], 1: [3, 4], 2: [4, 5, 6]}
