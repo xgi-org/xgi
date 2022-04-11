fix: #74 wrong dimension for degree when no neighbours (#75)
fix: #74 wrong dimension for degree when no neighbours

xgi/linalg/matrix.py
@@ -5,6 +5,7 @@ __all__ = [
     "incidence_matrix",
     "adjacency_matrix",
     "intersection_profile",
+    "degree_matrix",
     "laplacian",
     "clique_motif_matrix",
 ]
@@ -204,7 +205,7 @@ def intersection_profile(H, order=None, index=False):
         return P
 
 
-def _degree(H, order=None, index=False):
+def degree_matrix(H, order=None, index=False):
     """Returns the degree of each node as an array
 
     Parameters
@@ -224,16 +225,13 @@ def _degree(H, order=None, index=False):
     else:
         return K
     """
-
-    if index:
-        I, rowdict, _ = incidence_matrix(H, index=True, order=order)
-    else:
-        I = incidence_matrix(H, index=False, order=order)
+    I, rowdict, _ = incidence_matrix(H, order=order, index=True)
 
     if I.shape == (0,):
-        return np.array([])
+        K = np.zeros(H.num_nodes)
+    else:
+        K = np.ravel(np.sum(I, axis=1))  # flatten
 
-    K = np.sum(I, axis=1)
     return (K, rowdict) if index else K
 
 
@@ -268,9 +266,9 @@ def laplacian(H, order=1, rescale_per_node=False, index=False):
     if A.shape == (0,):
         return (np.array([]), {}) if index else np.array([])
 
-    K = _degree(H, order=order, index=False)
+    K = degree_matrix(H, order=order, index=False)
 
-    L = order * np.diag(np.ravel(K)) - A  # ravel needed to convert sparse matrix
+    L = order * np.diag(K) - A  # ravel needed to convert sparse matrix
     L = np.asarray(L)
 
     if rescale_per_node: