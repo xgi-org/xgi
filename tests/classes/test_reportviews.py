3cda3e66cd50c3de2f2e00ba98c05586ba40eaac
nwlandry <nicholas.landry.91@gmail.com>
2022-04-11 14:58:32 -0600

    style: format with black

--- tests/classes/test_reportviews.py
+++ tests/classes/test_reportviews.py
@@ -3,7 +3,8 @@ import xgi
 from xgi.exception import XGIError
 import numpy as np
 
-def test_id_degree_view(edgelist1,edgelist4):
+
+def test_id_degree_view(edgelist1, edgelist4):
     H1 = xgi.Hypergraph(edgelist4)
     H2 = xgi.Hypergraph(edgelist1)
     # test basic functionality
@@ -12,22 +13,22 @@ def test_id_degree_view(edgelist1,edgelist4):
     assert H1.degree(3) == 3
     with pytest.raises(XGIError):
         H1.degree(0)
-    
-    assert H1.edge_size()[H1.edges] == {0:3, 1:4, 2:3}
-    assert H2.edge_size()[H2.edges] == {0:3, 1:1, 2:2, 3:3}
+
+    assert H1.edge_size()[H1.edges] == {0: 3, 1: 4, 2: 3}
+    assert H2.edge_size()[H2.edges] == {0: 3, 1: 1, 2: 2, 3: 3}
 
     # check len
     assert len(H1.degree([1, 2])) == 2
     assert len(H1.degree()) == 5
 
     # check string
-    assert str(H1.degree()) == '[1, 2, 3, 4, 5]'
+    assert str(H1.degree()) == "[1, 2, 3, 4, 5]"
 
-    #check representation
-    assert repr(H1.degree()) == 'DegreeView({1: 1, 2: 2, 3: 3, 4: 2, 5: 2})'
+    # check representation
+    assert repr(H1.degree()) == "DegreeView({1: 1, 2: 2, 3: 3, 4: 2, 5: 2})"
 
     # check __iter__
-    assert {id : deg for id, deg in H1.degree()} == {1:1, 2:2, 3:3, 4:2, 5:2}
+    assert {id: deg for id, deg in H1.degree()} == {1: 1, 2: 2, 3: 3, 4: 2, 5: 2}
 
     # check dtype parameter
     assert H1.degree(dtype="list")[H1.nodes] == [1, 2, 3, 2, 2]
@@ -48,14 +49,16 @@ def test_id_degree_view(edgelist1,edgelist4):
     assert H1.degree(5, order=3) == 1
 
     # test weights
-    attr_dict1 = {
-        0: {"weight": -2},
-        1: {"weight": 4.0},
-        2: {"weight": 0.3}
-    }
+    attr_dict1 = {0: {"weight": -2}, 1: {"weight": 4.0}, 2: {"weight": 0.3}}
     xgi.set_edge_attributes(H1, attr_dict1)
 
     [[1, 2, 3], [2, 3, 4, 5], [3, 4, 5]]
 
-    assert H1.degree(weight="weight")[H1.nodes] == {1:-2, 2:2, 3:2.3, 4:4.3, 5:4.3}
-    assert H1.degree(weight="weight", order=3)[H1.nodes] == {1:-2, 2:-2, 3:-1.7, 4:0.3, 5:0.3}
+    assert H1.degree(weight="weight")[H1.nodes] == {1: -2, 2: 2, 3: 2.3, 4: 4.3, 5: 4.3}
+    assert H1.degree(weight="weight", order=3)[H1.nodes] == {
+        1: -2,
+        2: -2,
+        3: -1.7,
+        4: 0.3,
+        5: 0.3,
+    }
