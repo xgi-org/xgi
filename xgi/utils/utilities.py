924d0e941feaaf2afbbecba06b089721ff9be3f3
nwlandry <nicholas.landry.91@gmail.com>
2022-04-18 15:29:55 -0600

    feat: added method to get xgi-data datasets

--- xgi/utils/utilities.py
+++ xgi/utils/utilities.py
@@ -1,7 +1,12 @@
 """General utilities."""
 from collections import defaultdict
 
-__all__ = ["XGICounter", "get_dual"]
+import requests
+
+import xgi
+from xgi.exception import XGIError
+
+__all__ = ["XGICounter", "get_dual", "load_xgi_data"]
 
 
 class XGICounter:
@@ -65,3 +70,102 @@ def get_dual(edge_dict):
             node_dict[node].append(edge_id)
 
     return dict(node_dict)
+
+
+dataset_urls = {
+    "congress-bills": "https://raw.githubusercontent.com/ComplexGroupInteractions/xgi-data/main/data/congress-bills/congress-bills.json",
+    "tags-ask-ubuntu": "https://raw.githubusercontent.com/ComplexGroupInteractions/xgi-data/main/data/tags-ask-ubuntu/tags-ask-ubuntu.json",
+    "email-eu": "https://raw.githubusercontent.com/ComplexGroupInteractions/xgi-data/main/data/email-Eu/email-Eu.json",
+    "email-enron": "https://raw.githubusercontent.com/ComplexGroupInteractions/xgi-data/main/data/email-Enron/email-Enron.json",
+}
+
+
+def load_xgi_data(dataset, nodetype=None, edgetype=None):
+
+    if dataset not in dataset_urls:
+        raise XGIError("Invalid dataset specifier!")
+
+    r = requests.get(dataset_urls[dataset])
+
+    return _dict_to_hypergraph(r.json(), nodetype=nodetype, edgetype=edgetype)
+
+def _dict_to_hypergraph(hypergraph_dict, nodetype=None, edgetype=None):
+    """
+    A function to read a file in a standardized JSON format.
+
+    Parameters
+    ----------
+    path: string
+        The path of the file to read from
+
+    Returns
+    -------
+    A Hypergraph object
+        The loaded hypergraph
+    nodetype: type
+        type that the node labels will be cast to
+    edgetype: type
+        type that the edge labels will be cast to
+
+    Raises
+    ------
+    XGIError
+        If the json is not in a format that can be loaded.
+
+    Examples
+    --------
+        >>> import xgi
+        >>> H = xgi.read_hypergraph_json("test.json")
+    """
+
+    H = xgi.empty_hypergraph()
+    try:
+        H._hypergraph.update(hypergraph_dict["hypergraph-data"])
+    except KeyError:
+        raise XGIError("Failed to get hypergraph data attributes.")
+
+    try:
+        for id, dd in hypergraph_dict["node-data"].items():
+            if nodetype is not None:
+                try:
+                    id = nodetype(id)
+                except Exception as e:
+                    raise TypeError(
+                        f"Failed to convert edge IDs to type {nodetype}."
+                    ) from e
+            H.add_node(id, **dd)
+    except KeyError:
+        raise XGIError("Failed to import node attributes.")
+
+    try:
+        for id, edge in hypergraph_dict["edge-dict"].items():
+            if edgetype is not None:
+                try:
+                    id = edgetype(id)
+                except Exception as e:
+                    raise TypeError(
+                        f"Failed to convert the edge with ID {id} to type {edgetype}."
+                    ) from e
+
+            if nodetype is not None:
+                try:
+                    edge = [nodetype(n) for n in edge]
+                except Exception as e:
+                    raise TypeError(
+                        f"Failed to convert nodes to type {nodetype}."
+                    ) from e
+            H.add_edge(edge, id)
+    except:
+        raise XGIError("Failed to import edge dictionary.")
+
+    try:
+        xgi.set_edge_attributes(
+            H,
+            hypergraph_dict["edge-data"]
+            if edgetype is None
+            else {edgetype(e): dd for e, dd in hypergraph_dict["edge-data"].items()},
+        )
+    except KeyError:
+        raise XGIError("Failed to import edge attributes.")
+
+    return H
