31fbd32e0f6286c146fcd82e1afec4c91ccc9134
nwlandry <nicholas.landry.91@gmail.com>
2022-06-14 10:14:18 -0400

    update

--- xgi/algorithms/assortativity.py
+++ xgi/algorithms/assortativity.py
@@ -6,9 +6,7 @@ import random
 
 __all__ = [
     "dynamical_assortativity",
-    "top_2_assortativity",
-    "top_bottom_assortativity",
-    "uniform_assortativity",
+    "degree_assortativity"
 ]
 
 def dynamical_assortativity(H):
@@ -32,35 +30,44 @@ def dynamical_assortativity(H):
     if not xgi.is_uniform(H):
         raise XGIError("Hypergraph must be uniform!")
     
-    k1 = np.mean([H.degree(n) for n in H.nodes])
-    k2 = np.mean([H.degree(n)**2 for n in H.nodes])
-
-    kk1 = np.mean([H.degree(n1)*H.degree(n2) for e in H.edges for n1, n2 in combinations(H.edges.members(e), 2)])
+    degs = H.degree()
+    k1 = np.mean([degs[n] for n in H.nodes])
+    k2 = np.mean([degs[n]**2 for n in H.nodes])
+    kk1 = np.mean([degs[n1]*degs[n2] for e in H.edges for n1, n2 in combinations(H.edges.members(e), 2)])
+    
     return kk1*k1**2/k2**2 - 1
 
 
-def top_2_assortativity(H):
-    degrees = dict(H.degree())
-    k1 = [max((degrees[n] for n in H.edges.members(e))) for e in H.edges]
-    try:
-        k2 = [sorted((degrees[n] for n in H.edges.members(e)))[-2] for e in H.edges]
-    except IndexError:
-        raise XGIError("Hypergraph must not have singleton edges.")
-    return np.corrcoef(k1, k2)[0, 1]
+def degree_assortativity(H, type="uniform", exact=True, num_samples=1000):
+    k = dict(H.degree())
+    if exact:
+        k1k2 = np.array([choose_nodes(e, k) for e in H.edges if len(H.edges.members(e)) > 1])
+    else:
+        # edges = list(H.edges)
+        # k1k2 = np.array([choose_nodes(e, k) for H.edges[random.randint(H.num_edges)] in range(num_samples) if len(H.edges.members(e)) > 1])
+        raise XGIError("not done yet")
+
+    return np.corrcoef(k1k2)[0, 1]
 
 
-def top_bottom_assortativity(H):
-    degrees = dict(H.degree())
-    k1 = [max((degrees[n] for n in H.edges.members(e))) for e in H.edges]
-    k2 = [min((degrees[n] for n in H.edges.members(e))) for e in H.edges]
-    return np.corrcoef(k1, k2)[0, 1]
-
-
-def uniform_assortativity(H):
-    degrees = dict(H.degree())
-    try:
-        k1k2 = [[degrees[n] for n in random.sample(H.edges.members(e), 2)] for e in H.edges]
-    except ValueError:
-        raise XGIError("Hypergraph must not have singleton edges.")
-
-    return np.corrcoef(list(zip(*k1k2)))[0, 1]
+def choose_nodes(e, k, type="uniform"):
+    if type == "uniform":
+        i = np.random.randint(len(e))
+        j = i
+        while i == j:
+            j = np.random.randint(len(e))
+        return (np.array([k[e[i]], k[e[j]]]))
+    
+    elif type == "":
+        degs = sorted([k[i] for i in e])[-2:]
+        random.shuffle(degs)
+        return (degs)
+    
+    elif type == "":
+        degs = sorted([k[i] for i in e])[::len(e)-1] 
+        random.shuffle(degs)
+        return (degs)
+    
+    else:
+        raise XGIError("Invalid choice function!")
+    
