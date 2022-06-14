78db5e565ad1041e8d9d868c7c084c79bf711cd6
nwlandry <nicholas.landry.91@gmail.com>
2022-06-14 13:34:35 -0400

    style: run isort and black

--- xgi/algorithms/assortativity.py
+++ xgi/algorithms/assortativity.py
@@ -57,7 +57,7 @@ def dynamical_assortativity(H):
 
 
 def degree_assortativity(H, type="uniform", exact=False, num_samples=1000):
-    """ Computes the degree assortativity of a hypergraph
+    """Computes the degree assortativity of a hypergraph
 
     Parameters
     ----------
@@ -76,7 +76,7 @@ def degree_assortativity(H, type="uniform", exact=False, num_samples=1000):
     -------
     float
         the degree assortativity
-    
+
     References
     ----------
     Phil Chodrow,
@@ -87,7 +87,9 @@ def degree_assortativity(H, type="uniform", exact=False, num_samples=1000):
     degs = H.degree()
     if exact:
         k1k2 = [
-            choose_nodes(e, degs, type) for e in H.edges if len(H.edges.members(e)) > 1
+            choose_degrees(e, degs, type)
+            for e in H.edges
+            if len(H.edges.members(e)) > 1
         ]
     else:
         edges = list([e for e in H.edges if len(H.edges.members(e)) > 1])
@@ -96,13 +98,13 @@ def degree_assortativity(H, type="uniform", exact=False, num_samples=1000):
         samples = 0
         while samples < num_samples:
             e = random.choice(edges)
-            k1k2.append(choose_nodes(H.edges.members(e), degs, type))
+            k1k2.append(choose_degrees(H.edges.members(e), degs, type))
             samples += 1
     return np.corrcoef(np.array(k1k2).T)[0, 1]
 
 
-def choose_nodes(e, k, type="uniform"):
-    """ Choose the degrees of two nodes in a hyperedge.
+def choose_degrees(e, k, type="uniform"):
+    """Choose the degrees of two nodes in a hyperedge.
 
     Parameters
     ----------
@@ -122,7 +124,7 @@ def choose_nodes(e, k, type="uniform"):
     ------
     XGIError
         if invalid assortativity function chosen
-    
+
     References
     ----------
     Phil Chodrow,
@@ -130,22 +132,25 @@ def choose_nodes(e, k, type="uniform"):
     Journal of Complex Networks 2020.
     DOI: 10.1093/comnet/cnaa018
     """
-    if type == "uniform":
-        i = np.random.randint(len(e))
-        j = i
-        while i == j:
-            j = np.random.randint(len(e))
-        return np.array([k[e[i]], k[e[j]]])
+    if len(e) > 1:
+        if type == "uniform":
+            i = np.random.randint(len(e))
+            j = i
+            while i == j:
+                j = np.random.randint(len(e))
+            return np.array([k[e[i]], k[e[j]]])
 
-    elif type == "top-2":
-        degs = sorted([k[i] for i in e])[-2:]
-        random.shuffle(degs)
-        return degs
+        elif type == "top-2":
+            degs = sorted([k[i] for i in e])[-2:]
+            random.shuffle(degs)
+            return degs
 
-    elif type == "top-bottom":
-        degs = sorted([k[i] for i in e])[:: len(e) - 1]
-        random.shuffle(degs)
-        return degs
+        elif type == "top-bottom":
+            degs = sorted([k[i] for i in e])[:: len(e) - 1]
+            random.shuffle(degs)
+            return degs
 
+        else:
+            raise XGIError("Invalid choice function!")
     else:
-        raise XGIError("Invalid choice function!")
+        raise XGIError("Edge must have more than one member!")
