8c12e6499b2523d8514e82a5abb61333b1a612d0
nwlandry <nicholas.landry.91@gmail.com>
2022-06-28 11:18:40 -0400

    moved load_xgi_data tests

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,17 +1,4 @@
-import pytest
-
-from xgi.utils import convert_labels_to_integers, dual_dict, load_xgi_data
-
-
-@pytest.mark.webtest
-@pytest.mark.slow
-def test_load_xgi_data():
-    H = load_xgi_data("email-enron")
-    assert H.num_nodes == 148
-    assert H.num_edges == 10885
-    assert H["name"] == "email-Enron"
-    assert H.nodes["4"]["name"] == "robert.badeer@enron.com"
-    assert H.edges["0"]["timestamp"] == "2000-01-11T10:29:00"
+from xgi.utils import convert_labels_to_integers, dual_dict
 
 
 def test_dual_dict(dict5):
