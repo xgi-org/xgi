bb0b972cd2f31fffb21fdbf8d3384c6022439ce4
nwlandry <nicholas.landry.91@gmail.com>
2022-06-29 18:07:19 -0400

    added unit tests

--- tests/stats/test_nodestats.py
+++ tests/stats/test_nodestats.py
@@ -1,8 +1,9 @@
+import numpy as np
 import pandas as pd
 import pytest
 
 import xgi
-from xgi.exception import IDNotFound
+from xgi.exception import IDNotFound, XGIError
 
 
 def test_filterby_wrong_stat():
@@ -533,3 +534,54 @@ def test_view_val(edgelist1, edgelist2):
     H = xgi.Hypergraph(edgelist2)
     assert H.nodes.degree._val == {1: 1, 2: 1, 3: 1, 4: 2, 5: 1, 6: 1}
     assert H.nodes([4, 5, 6]).degree._val == {4: 2, 5: 1, 6: 1}
+
+
+def test_cec_centrality():
+    H = xgi.sunflower(3, 1, 3)
+    c = H.nodes.cec_centrality.asnumpy()
+    assert np.all(np.abs(c[1:] - c[1]) < 1e-6)
+    assert abs(c[0]/c[1] - ratio(3, 3)) < 1e-6
+
+    H = xgi.sunflower(5, 1, 7)
+    c = H.nodes.cec_centrality.asnumpy()
+    assert np.all(np.abs(c[1:] - c[1]) < 1e-6)
+    assert abs(c[0]/c[1] - ratio(5, 7)) < 1e-6
+
+
+def ratio(r, m, kind="CEC"):
+    """ Generate the ratio between largest and second largest centralities 
+    for the sunflower hypergraph with one core node.
+
+    Parameters
+    ----------
+    r : int
+        Number of petals
+    m : int
+        Size of edges
+    kind : str, default: "CEC"
+        "CEC", "HEC", or "ZEC"
+
+    Returns
+    -------
+    float
+        Ratio
+
+    Raises
+    ------
+    XGIError
+        If edge size is too small for ZEC
+    
+    References
+    ----------
+    Three Hypergraph Eigenvector Centralities,
+    Austin R. Benson,
+    https://doi.org/10.1137/18M1203031
+    """
+    if kind == "CEC":
+        return 2*r*(m - 1)/(np.sqrt(m**2 + 4*(m - 1)*(r - 1)) + m - 2)
+    elif kind == "ZEC":
+        if m > 4:
+            raise XGIError("Choose a larger m value.")
+        return r**0.5
+    elif kind == "HEC":
+        return r**(1.0/m)
