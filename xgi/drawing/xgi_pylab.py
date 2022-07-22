3566e18336be1fd8c38ce1ff10b0e5cb6fd19cd0
nwlandry <nicholas.landry.91@gmail.com>
2022-07-22 12:18:35 -0400

    various fixes

--- xgi/drawing/xgi_pylab.py
+++ xgi/drawing/xgi_pylab.py
@@ -31,10 +31,11 @@ def draw(
     ax=None,
     edge_lc="black",
     edge_lw=1.5,
+    edge_fc=None,
     node_fc="white",
     node_ec="black",
-    node_lw=100,
-    node_size=3,
+    node_lw=1,
+    node_size=10,
 ):
     """Draw hypergraph or simplicial complex.
 
@@ -72,10 +73,10 @@ def draw(
     specified in the same order as the nodes are found in H.nodes.
 
     node_lw : float (default=1.0)
-    Line width of the node borders.
+    Line width of the node borders in pixels.
 
     node_size : float (default=0.03)
-    Size of the nodes.
+    Radius of the nodes in pixels
 
     Examples
     --------
@@ -89,20 +90,6 @@ def draw(
     if pos is None:
         pos = barycenter_spring_layout(H)
 
-    # Defining colors, one for each dimension
-    d_max = max_edge_order(H)
-    if cmap is None:
-        cmap = cm.Paired
-        colors = [cmap(i) for i in range(0, d_max + 1)]
-    else:
-        if type(cmap) == ListedColormap:
-            # The colormap is already discrete
-            colors = [cmap(i) for i in range(0, d_max + 1)]
-        elif type(cmap) == LinearSegmentedColormap:
-            # I need to discretize the given colormap
-            color_range = np.linspace(0.1, 0.9, d_max)
-            colors = [cmap(i) for i in color_range]
-
     if ax is None:
         ax = plt.gca()
     ax.set_xlim([-1.1, 1.1])
@@ -111,16 +98,17 @@ def draw(
     ax.get_yaxis().set_ticks([])
     ax.axis("off")
 
+    d_max = max_edge_order(H)
+
     if isinstance(H, Hypergraph):
-        draw_xgi_hyperedges(ax, H, pos, edge_lc, edge_lw, d_max, colors)
+        draw_xgi_hyperedges(ax, H, pos, edge_lc, edge_lw, edge_fc, d_max)
 
     elif isinstance(H, SimplicialComplex):
-        # I will only plot the maximal simplices, so I convert the SC to H
-        draw_xgi_complexes(ax, H, pos, edge_lc, edge_lw, d_max, colors)
+        draw_xgi_complexes(ax, H, pos, edge_lc, edge_lw, edge_fc, d_max)
     else:
         raise XGIError("The input must be a SimplicialComplex or Hypergraph")
 
-    draw_xgi_nodes(ax, H, pos, node_fc, node_ec, node_size, node_lw, d_max + 1)
+    draw_xgi_nodes(ax, H, pos, node_fc, node_ec, node_size, node_lw, d_max)
 
 
 def draw_xgi_nodes(ax, H, pos, node_fc, node_ec, node_size, node_lw, zorder):
@@ -134,82 +122,86 @@ def draw_xgi_nodes(ax, H, pos, node_fc, node_ec, node_size, node_lw, zorder):
         ax.scatter(
             x,
             y,
-            s=node_size[i],
+            s=node_size[i] ** 2,
             c=node_fc[i],
             edgecolors=node_ec[i],
             linewidths=node_lw,
             zorder=zorder,
         )
-        
 
 
-def draw_xgi_hyperedges(ax, H, pos, edge_lc, edge_lw, d_max, colors):
+def draw_xgi_hyperedges(ax, H, pos, edge_lc, edge_lw, edge_fc, d_max):
     edge_lc = _arg_to_dict(edge_lc, H.edges)
+    edge_lw = _arg_to_dict(edge_lw, H.edges)
+
+    edge_fc = _color_edges(edge_fc, H)
     # Looping over the hyperedges of different order (reversed) -- nodes will be plotted separately
-    for d in reversed(range(1, d_max + 1)):
+
+    for id, he in H.edges.members(dtype=dict).items():
+        d = len(he) - 1
         if d == 1:
             # Drawing the edges
-            for idx, he in H.edges.filterby("order", d).members(dtype=dict).items():
-                he = list(he)
-                x_coords = [pos[he[0]][0], pos[he[1]][0]]
-                y_coords = [pos[he[0]][1], pos[he[1]][1]]
-                line = plt.Line2D(x_coords, y_coords, color=edge_lc[idx], lw=edge_lw)
-                ax.add_line(line)
+            he = list(he)
+            x_coords = [pos[he[0]][0], pos[he[1]][0]]
+            y_coords = [pos[he[0]][1], pos[he[1]][1]]
+            line = plt.Line2D(
+                x_coords, y_coords, color=edge_lc[id], lw=edge_lw[id], zorder=d_max - 1
+            )
+            ax.add_line(line)
 
         else:
             # Hyperedges of order d (d=1: links, etc.)
-            for idx, he in H.edges.filterby("order", d).members(dtype=dict).items():
-                # Filling the polygon
-                coordinates = [[pos[n][0], pos[n][1]] for n in he]
-                # Sorting the points counterclockwise (needed to have the correct filling)
-                sorted_coordinates = _CCW_sort(coordinates)
-                obj = plt.Polygon(
-                    sorted_coordinates,
-                    edgecolor=edge_lc[idx],
-                    facecolor=colors[d - 1],
-                    alpha=0.4,
-                    lw=0.5,
-                )
-                ax.add_patch(obj)
+            # Filling the polygon
+            coordinates = [[pos[n][0], pos[n][1]] for n in he]
+            # Sorting the points counterclockwise (needed to have the correct filling)
+            sorted_coordinates = _CCW_sort(coordinates)
+            obj = plt.Polygon(
+                sorted_coordinates,
+                edgecolor=edge_lc[id],
+                facecolor=edge_fc[id],
+                alpha=0.4,
+                lw=0.5,
+                zorder=d_max - d,
+            )
+            ax.add_patch(obj)
 
 
-def draw_xgi_complexes(ax, H, pos, edge_lc, edge_lw, colors, d_max):
+def draw_xgi_complexes(ax, H, pos, edge_lc, edge_lw, edge_fc, d_max):
+    # I will only plot the maximal simplices, so I convert the SC to H
     H_ = convert.from_simplicial_complex_to_hypergraph(H)
 
     edge_lc = _arg_to_dict(edge_lc, H.edges)
+    edge_fc = _color_edges(edge_fc, H.edges)
     # Looping over the hyperedges of different order (reversed) -- nodes will be plotted separately
-    for d in reversed(range(1, d_max + 1)):
+    for id, he in H_.edges.members(dtype=dict).items():
+        d = len(he) - 1
         if d == 1:
             # Drawing the edges
-            for idx, he in H_.edges.filterby("order", d).members(dtype=dict).items():
-                he = list(he)
-                x_coords = [pos[he[0]][0], pos[he[1]][0]]
-                y_coords = [pos[he[0]][1], pos[he[1]][1]]
-                line = plt.Line2D(x_coords, y_coords, color=edge_lc[idx], lw=edge_lw)
-                ax.add_line(line)
+            he = list(he)
+            x_coords = [pos[he[0]][0], pos[he[1]][0]]
+            y_coords = [pos[he[0]][1], pos[he[1]][1]]
+            line = plt.Line2D(x_coords, y_coords, color=edge_lc[id], lw=edge_lw)
+            ax.add_line(line)
         else:
             # Hyperedges of order d (d=1: links, etc.)
-            for idx, he in H_.edges.filterby("order", d).members(dtype=dict).items():
-                # Filling the polygon
-                coordinates = [[pos[n][0], pos[n][1]] for n in he]
-                # Sorting the points counterclockwise (needed to have the correct filling)
-                sorted_coordinates = _CCW_sort(coordinates)
-                obj = plt.Polygon(
-                    sorted_coordinates,
-                    edgecolor=edge_lc[idx],
-                    facecolor=colors[d - 1],
-                    alpha=0.4,
-                    lw=0.5,
-                )
-                ax.add_patch(obj)
-                # Drawing the all the edges within
-                for i, j in combinations(sorted_coordinates, 2):
-                    x_coords = [i[0], j[0]]
-                    y_coords = [i[1], j[1]]
-                    line = plt.Line2D(
-                        x_coords, y_coords, color=edge_lc[idx], lw=edge_lw
-                    )
-                    ax.add_line(line)
+            # Filling the polygon
+            coordinates = [[pos[n][0], pos[n][1]] for n in he]
+            # Sorting the points counterclockwise (needed to have the correct filling)
+            sorted_coordinates = _CCW_sort(coordinates)
+            obj = plt.Polygon(
+                sorted_coordinates,
+                edgecolor=edge_lc[id],
+                facecolor=edge_fc[id],
+                alpha=0.4,
+                lw=0.5,
+            )
+            ax.add_patch(obj)
+            # Drawing the all the edges within
+            for i, j in combinations(sorted_coordinates, 2):
+                x_coords = [i[0], j[0]]
+                y_coords = [i[1], j[1]]
+                line = plt.Line2D(x_coords, y_coords, color=edge_lc[id], lw=edge_lw[id])
+                ax.add_line(line)
 
 
 def _arg_to_dict(arg, ids):
@@ -242,6 +234,46 @@ def _arg_to_dict(arg, ids):
         raise TypeError(
             f"argument must be dict, str, or iterable, received {type(arg)}"
         )
+
+
+def _color_edges(colors, H):
+    """Map different types of arguments for drawing style to a dict.
+
+    Parameters
+    ----------
+    colors : str, dict, cmap, or iterable
+        attributes for drawing parameter
+    ids : NodeView or EdgeView
+        This is the node or edge IDs that attributes get mapped to.
+
+    Returns
+    -------
+    dict
+        an ID: attribute dictionary
+
+    Raises
+    ------
+    TypeError
+        if a string, list, or dict is not passed
+    """
+    # Defining colors, one for each dimension
+    d_max = max_edge_order(H)
+    if colors is None:
+        colors = cm.Paired
+
+    if type(colors) == ListedColormap:
+        # The colormap is already discrete
+        c = [colors(i) for i in range(d_max)]
+        order = H.edges.order.asdict()
+        return {e: c[order[e] - 1] for e in H.edges}
+    elif type(colors) == LinearSegmentedColormap:
+        # I need to discretize the given colormap
+        color_range = np.linspace(0.1, 0.9, d_max)
+        c = [colors(i) for i in color_range]
+        order = H.edges.order.asdict()
+        return {e: c[order[e] - 1] for e in H.edges}
+    else:
+        return _arg_to_dict(colors, H.edges)
 
 
 def _CCW_sort(p):
