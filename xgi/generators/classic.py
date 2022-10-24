6ca1bae21e6ba6b44467df2f7d4f941c804b293d
Maxime Lucas <maximelucas@users.noreply.github.com>
2022-10-24 17:12:41 +0200

    minor rewrite
    Co-authored-by: Leo Torres <leo@leotrs.com>

--- xgi/generators/classic.py
+++ xgi/generators/classic.py
@@ -219,14 +219,17 @@ def flag_complex(G, max_order=2, ps=None, seed=None):
     S.add_simplices_from(edges)
     if not ps: # promote all cliques
         S.add_simplices_from(max_cliques, max_order=max_order)
-    else: # promote cliques with a given probability
-        cliques_d = defaultdict(list)
-        for x in max_cliques:
-            cliques_d[len(x)].append(x)
-        for i, p in enumerate(ps[:max_order - 1]):
-            d = i + 2 # simplex order
-            cliques_d_to_add = [el for el in cliques_d[d+1] if seed.random() <= p]
-            S.add_simplices_from(cliques_d_to_add, max_order=max_order)
+        return S
+        
+    # promote cliques with a given probability
+    cliques_d = defaultdict(list)
+    for x in max_cliques:
+        cliques_d[len(x)].append(x)
+            
+    for i, p in enumerate(ps[:max_order - 1]):
+        d = i + 2 # simplex order
+        cliques_d_to_add = [el for el in cliques_d[d+1] if seed.random() <= p]
+        S.add_simplices_from(cliques_d_to_add, max_order=max_order)
 
     return S
 
