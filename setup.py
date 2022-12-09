23cc3729473552bcf28b27a614e813c8f500e73e
Nicholas Landry <nicholas.landry.91@gmail.com>
2022-12-09 17:43:44 -0500

    Increased version number to 0.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 import setuptools
 from setuptools import setup
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 if sys.version_info < (3, 7):
     sys.exit("XGI requires Python 3.7 or later.")
