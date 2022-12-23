3f6c4e3c0dd9b5578fe79ae0a855ea18da519d51
Nicholas Landry <nicholas.landry.91@gmail.com>
2022-12-23 12:19:17 -0500

    modified the XGI-DATA request

--- xgi/readwrite/xgi_data.py
+++ xgi/readwrite/xgi_data.py
@@ -1,5 +1,6 @@
 import json
 import os
+from functools import lru_cache
 from warnings import warn
 
 import requests
@@ -11,7 +12,13 @@ __all__ = ["load_xgi_data", "download_xgi_data"]
 
 
 def load_xgi_data(
-    dataset, path="", read=True, nodetype=None, edgetype=None, max_order=None
+    dataset,
+    cache=True,
+    read=False,
+    path="",
+    nodetype=None,
+    edgetype=None,
+    max_order=None,
 ):
     """Load a data set from the xgi-data repository or a local file.
 
@@ -20,12 +27,13 @@ def load_xgi_data(
     dataset : str
         Dataset name. Valid options are the top-level tags of the
         index.json file in the xgi-data repository.
-
-    path : str, optional
-        Path to a local copy of the data set
+    cache : bool, optional
+        Whether to cache the input data
     read : bool, optional
         If read==True, search for a local copy of the data set. Use the local
         copy if it exists, otherwise use the  xgi-data repository.
+    path : str, optional
+        Path to a local copy of the data set
     nodetype : type, optional
         Type to cast the node ID to
     edgetype : type, optional
@@ -48,11 +56,16 @@ def load_xgi_data(
         cfp = os.path.join(path, dataset + ".json")
         if os.path.exists(cfp):
             data = json.load(open(cfp, "r"))
+
+            return convert.dict_to_hypergraph(
+                data, nodetype=nodetype, edgetype=edgetype, max_order=max_order
+            )
         else:
             warn(
                 f"No local copy was found at {cfp}. The data is requested from the xgi-data repository instead. To download a local copy, use `download_xgi_data`."
             )
-            data = _request_from_xgi_data(dataset)
+    if cache:
+        data = _request_from_xgi_data_cached(dataset)
     else:
         data = _request_from_xgi_data(dataset)
 
@@ -101,14 +114,46 @@ def _request_from_xgi_data(dataset):
     """
 
     index_url = "https://gitlab.com/complexgroupinteractions/xgi-data/-/raw/main/index.json?inline=false"
-    index = requests.get(index_url).json()
+    r = requests.get(index_url)
+
+    if r.ok:
+        index_data = r.json()
+    else:
+        raise XGIError("Error: HTTP response {r.status_code}")
 
     key = dataset.lower()
-    if key not in index:
+    if key not in index_data:
         print("Valid dataset names:")
-        print(*index, sep="\n")
+        print(*index_data, sep="\n")
         raise XGIError("Must choose a valid dataset name!")
 
-    data = requests.get(index[key]["url"]).json()
+    r = requests.get(index_data[key]["url"])
+    if r.ok:
+        return r.json()
+    else:
+        raise XGIError("Error: HTTP response {r.status_code}")
 
-    return data
+
+@lru_cache(maxsize=None)
+def _request_from_xgi_data_cached(dataset):
+    """Request a dataset from xgi-data and cache the result.
+
+    Wraps `_request_from_xgi_data` in an lru_cache decorator.
+
+     Parameters
+     ----------
+     dataset : str
+         Dataset name. Valid options are the top-level tags of the
+         index.json file in the xgi-data repository.
+
+     Returns
+     -------
+     Data
+         The requested data loaded from a json file.
+
+    See also
+    ---------
+    load_xgi_data
+    """
+
+    return _request_from_xgi_data(dataset)
