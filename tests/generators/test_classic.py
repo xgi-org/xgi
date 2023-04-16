b90f4779731e4b918bf3bc7cb4fea5f9400331d7
Nicholas Landry <nicholas.landry.91@gmail.com>
2023-04-16 17:15:57 -0400

    update quickstart notebook

--- tests/generators/test_classic.py
+++ tests/generators/test_classic.py
@@ -10,6 +10,7 @@ def test_empty_hypergraph():
     SC = xgi.empty_simplicial_complex()
     assert (SC.num_nodes, SC.num_edges) == (0, 0)
 
+
 def test_trivial_hypergraph():
     H = xgi.trivial_hypergraph()
     assert (H.num_nodes, H.num_edges) == (1, 0)
