5ffeafd0435d304d25b15e69a40c545260d41ae3
Nicholas Landry <nicholas.landry.91@gmail.com>
2023-05-03 18:44:42 -0400

    switched lists to sets

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -5,15 +5,15 @@ import xgi
 
 def test_dual_dict(dict5):
     dual = xgi.dual_dict(dict5)
-    assert dual[0] == [0]
-    assert dual[1] == [0]
-    assert dual[2] == [0]
-    assert dual[3] == [0]
-    assert dual[4] == [1]
-    assert dual[5] == [2]
-    assert dual[6] == [2, 3]
-    assert dual[7] == [3]
-    assert dual[8] == [3]
+    assert dual[0] == {0}
+    assert dual[1] == {0}
+    assert dual[2] == {0}
+    assert dual[3] == {0}
+    assert dual[4] == {1}
+    assert dual[5] == {2}
+    assert dual[6] == {2, 3}
+    assert dual[7] == {3}
+    assert dual[8] == {3}
 
 
 def test_powerset():
--- xgi/utils/utilities.py
+++ xgi/utils/utilities.py
@@ -13,32 +13,32 @@ __all__ = [
 
 def dual_dict(edge_dict):
     """Given a dictionary with IDs as keys
-    and lists as values, return the dual.
+    and sets as values, return the dual.
 
     Parameters
     ----------
     edge_dict : dict
         A dictionary where the keys are
-        IDs and the values are lists of hashables
+        IDs and the values are sets of hashables
 
     Returns
     -------
     dict
         A dictionary with IDs as keys
-        and lists as values, but the reverse of
+        and sets as values, but the reverse of
         the original dict.
 
     Examples
     --------
     >>> import xgi
     >>> xgi.dual_dict({0 : [1, 2, 3], 1 : [0, 2]})
-    {1: [0], 2: [0, 1], 3: [0], 0: [1]}
+    {1: {0}, 2: {0, 1}, 3: {0}, 0: {1}}
 
     """
-    node_dict = defaultdict(list)
+    node_dict = defaultdict(set)
     for edge_id, members in edge_dict.items():
         for node in members:
-            node_dict[node].append(edge_id)
+            node_dict[node].add(edge_id)
 
     return dict(node_dict)
 
