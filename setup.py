225034f43bd88fe06827c757bdd2d07d6742db03
Nicholas Landry <nicholas.landry.91@gmail.com>
2023-05-15 18:11:59 -0400

    second attempt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 import setuptools
 from setuptools import setup
 
-__version__ = "0.5.8.2"
+__version__ = "0.5.8.3"
 
 if sys.version_info < (3, 8):
     sys.exit("XGI requires Python 3.8 or later.")
