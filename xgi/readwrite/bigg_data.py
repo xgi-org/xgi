bd3c1dacd2fe05436cd31502f1d2719d8f12f6d3
Nicholas Landry <nicholas.landry.91@gmail.com>
2023-06-08 08:00:49 -0400

    added spaces

--- xgi/readwrite/bigg_data.py
+++ xgi/readwrite/bigg_data.py
@@ -90,8 +90,10 @@ def _bigg_to_dihypergraph(d_index, d_model):
     id = d_model["id"]
 
     DH["name"] = id
+    
     info = next((item for item in d_index["results"] if item["bigg_id"] == id), None)
     DH["organism"] = info["organism"]
+    
     for m in d_model["metabolites"]:
         DH.add_node(m["id"], name=m["name"])
 
