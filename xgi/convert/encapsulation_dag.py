187eb4fefe05e63d502dd3ce42109512db585ce6
Timothy LaRock <timothylarock@gmail.com>
2023-08-04 11:57:32 +0100

    Updates to documentation.

--- xgi/convert/encapsulation_dag.py
+++ xgi/convert/encapsulation_dag.py
@@ -5,7 +5,8 @@ import networkx as nx
 __all__ = ["to_encapsulation_dag", "empirical_subsets_filter"]
 
 def to_encapsulation_dag(H, subset_types="all"):
-    """The encapsulation DAG of the hypergraph.
+    """The encapsulation DAG (Directed Acyclic Graph) of
+    the hypergraph H.
 
     An encapsulation DAG is a directed line graph
     where the nodes are hyperedges in H and a directed edge
@@ -21,15 +22,33 @@ def to_encapsulation_dag(H, subset_types="all"):
             "all" : all subset relationships
             "immediate" : only subset relationships between hyperedges of
                 adjacent sizes (i.e., edges from k to k-1)
-            "empirical" : only subset relationships between hyperedges of
-                minimum size difference k and k'. A relaxation of the
-                "immediate" option. For example, a hyperedge of size 4 may
-                have no immediate encapsulation relationships with hyperedges
-                of size 3, but may encapsulate hyperedegs of size 2.
+            "empirical" : A relaxation of the "immediate" option where only
+                subset relationships between hyperedges of size k and subsets
+                of maximum size k'<k existing in the hypergraph are included.
+                For example, a hyperedge of size 5 may have no immediate
+                encapsulation relationships with hyperedges of size 4, but may
+                encapsulate hyperedegs of size 3, which will be included if
+                using this setting (whereas relationships with subsets of size 2
+                would not be included).
     Returns
     -------
     LG : networkx.DiGraph
          The line graph associated to the Hypergraph
+
+    Examples
+    --------
+    >>> import xgi
+    >>> from xgi.convert import to_encapsulation_dag, empirical_subsets_filter
+    >>> H = xgi.Hypergraph([["a","b","c"], ["b","c","f"], ["a","b"], ["c", "e"], ["a"], ["f"]])
+    >>> dag = to_encapsulation_dag(H)
+    >>> dag.edges()
+    OutEdgeView([(0, 2), (0, 4), (2, 4), (1, 5)])
+    >>> dag = to_encapsulation_dag(H, subset_types="immediate")
+    >>> dag.edges()
+    OutEdgeView([(0, 2), (2, 4)])
+    >>> dag = to_encapsulation_dag(H, subset_types="empirical")
+    >>> dag.edges()
+    OutEdgeView([(0, 2), (2, 4), (1, 5)])
 
     References
     ----------
@@ -37,7 +56,6 @@ def to_encapsulation_dag(H, subset_types="all"):
     & Renaud Lambiotte. https://arxiv.org/abs/2307.04613
 
     """
-
     if not (subset_types in ["all", "immediate", "empirical"]):
         raise XGIError(f"{subset_types} not a valid subset_types option. Choices are "
                        "'all', 'immediate', and 'empirical'.")
@@ -72,29 +90,77 @@ def to_encapsulation_dag(H, subset_types="all"):
 
     return dag
 
-
 def _encapsulated(larger, smaller):
+    """Test if a larger hyperedge encapsulates a smaller hyperedge.
+
+    Parameters
+    ----------
+    larger : Set
+        larger hyperedge
+    smaller : Set
+        smaller hyperedge
+    Returns
+    -------
+    _ : Bool
+        True if the size of the intersection between larger and smaller is
+            the same as the size of smaller.
+
+    """
     return len(set(larger).intersection(set(smaller))) == len(smaller)
 
-def _get_candidates(relations, H, he):
+def _get_candidates(subset_types, H, he):
+    """Get the candidate hyperedges for encapsulation by he based on the subset type.
+
+    Parameters
+    ----------
+    subset_types : str
+        Type of subset relationships
+    H : Hypergraph
+        The hypergraph
+    he : Set
+        The hyperedge
+    Returns
+    -------
+    candidates : Set
+        A set of hyperedge IDs to check for encapsulation by hyperedge he
+
+    """
     candidates = set()
     for node in he:
         for cand_idx in H.nodes.memberships(node):
             if cand_idx not in candidates:
                 cand = H.edges.members(cand_idx)
-                if _check_candidate(relations, he, cand):
+                if _check_candidate(subset_types, he, cand):
                     candidates.add(cand_idx)
 
     return candidates
 
 def _check_candidate(relations, he, cand):
+    """Check whether a hyperedge cand is a candidate to be encapsulated by
+        hyperedge he based on subset_types.
+
+        Parameters
+        ----------
+        subset_types : str
+            Type of subset relationships
+        he : Set
+            The hyperedge
+        cand : Set
+            The candidate
+        Returns
+        -------
+        is_candidate : bool
+            True if cand is a valid candidate to be encapsulated by he, False
+            otherwise.
+    """
+    is_candidate = False
     if relations in ["all", "empirical"]:
         if len(he) != len(cand):
-            return True
+            is_candidate = True
     elif relations == "immediate":
         if len(he) == len(cand)-1 or len(he)-1 == len(cand):
-            return True
-    return False
+            is_candidate = True
+    return is_candidate
 
 def empirical_subsets_filter(H, dag):
     """
