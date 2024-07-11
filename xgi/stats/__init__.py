340162ff81f6dd1b69f261921792efa27beccd9b
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-07-11 11:24:13 -0400

    Update __init__.py

--- xgi/stats/__init__.py
+++ xgi/stats/__init__.py
@@ -68,7 +68,6 @@ class IDStat:
     def __init__(self, network, view, func, args=None, kwargs=None):
         self.view = view
         self.net = network
-        print(args)
         self.args = () if args is None else args
         self.kwargs = {} if kwargs is None else kwargs
         self.func = func
