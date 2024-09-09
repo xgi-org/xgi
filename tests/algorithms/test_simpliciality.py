171783de2a4a61d3c60012dcfd3d8308eeb6a127
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-09-09 01:55:07 -0400

    added unit tests

--- tests/algorithms/test_simpliciality.py
+++ tests/algorithms/test_simpliciality.py
@@ -1,5 +1,6 @@
-import xgi
 import numpy as np
+
+import xgi
 
 
 def test_edit_simpliciality(
@@ -65,6 +66,74 @@ def test_edit_simpliciality(
     assert np.allclose(es, s / (m + s))
 
 
+def test_simplicial_edit_distance(
+    sc1_with_singletons,
+    h_missing_one_singleton,
+    h_missing_one_link,
+    h_links_and_triangles2,
+    h1,
+):
+    # simplicial complex
+    sed = xgi.simplicial_edit_distance(sc1_with_singletons)
+    assert sed == 0.0
+
+    sed = xgi.simplicial_edit_distance(sc1_with_singletons, min_size=1)
+    assert sed == 0.0
+
+    sed = xgi.simplicial_edit_distance(
+        sc1_with_singletons, min_size=1, exclude_min_size=False
+    )
+    assert sed == 0.0
+
+    # h1
+    sed = xgi.simplicial_edit_distance(h_missing_one_singleton)
+    assert sed == 0.0
+
+    sed = xgi.simplicial_edit_distance(h_missing_one_singleton, min_size=1)
+    assert np.allclose(sed, 1 / 7)
+
+    sed = xgi.simplicial_edit_distance(
+        h_missing_one_singleton, min_size=1, exclude_min_size=False
+    )
+    assert np.allclose(sed, 1 / 7)
+
+    # h2
+    sed = xgi.simplicial_edit_distance(h_missing_one_link)
+    assert np.allclose(sed, 1 / 4)
+
+    sed = xgi.simplicial_edit_distance(h_missing_one_link, min_size=1)
+    assert np.allclose(sed, 1 / 7)
+
+    # links and triangles 2
+    sed = xgi.simplicial_edit_distance(h_links_and_triangles2)
+    assert np.allclose(sed, 1 / 4)
+
+    sed = xgi.simplicial_edit_distance(h_links_and_triangles2, min_size=1)
+    assert np.allclose(sed, 1 / 2)
+
+    sed = xgi.simplicial_edit_distance(h_links_and_triangles2, exclude_min_size=False)
+    assert np.allclose(sed, 1 / 4)
+
+    # test h1
+    sed = xgi.simplicial_edit_distance(h1)
+    s = 4
+    m = 4 + 10
+    assert np.allclose(sed, m / (m + s))
+
+    sed = xgi.simplicial_edit_distance(h1, min_size=1)
+    s = 4
+    m = 4 + 10 + 7
+    assert np.allclose(sed, m / (m + s))
+
+    sed = xgi.simplicial_edit_distance(h1, exclude_min_size=False)
+    s = 4
+    m = 4 + 10
+    assert np.allclose(sed, m / (m + s))
+
+    sed = xgi.simplicial_edit_distance(h1, exclude_min_size=False, normalize=False)
+    assert np.allclose(sed, m)
+
+
 def test_face_edit_simpliciality(
     sc1_with_singletons,
     h_missing_one_singleton,
@@ -111,6 +180,54 @@ def test_face_edit_simpliciality(
 
     fes = xgi.face_edit_simpliciality(h_links_and_triangles2, exclude_min_size=False)
     assert np.allclose(fes, 7 / 9)
+
+
+def test_mean_face_edit_distance(
+    sc1_with_singletons,
+    h_missing_one_singleton,
+    h_missing_one_link,
+    h_links_and_triangles2,
+):
+    # simplicial complex
+    mfed = xgi.mean_face_edit_distance(sc1_with_singletons)
+    assert mfed == 0.0
+
+    mfed = xgi.mean_face_edit_distance(sc1_with_singletons, min_size=1)
+    assert mfed == 0.0
+
+    mfed = xgi.mean_face_edit_distance(
+        sc1_with_singletons, min_size=1, exclude_min_size=False
+    )
+    assert mfed == 0.0
+
+    # h1
+    mfed = xgi.mean_face_edit_distance(h_missing_one_singleton)
+    assert mfed == 0.0
+
+    mfed = xgi.mean_face_edit_distance(h_missing_one_singleton, min_size=1)
+    assert np.allclose(mfed, 1 / 6)
+
+    mfed = xgi.mean_face_edit_distance(
+        h_missing_one_singleton, min_size=1, exclude_min_size=False
+    )
+    assert np.allclose(mfed, 1 / 6)
+
+    # h2
+    mfed = xgi.mean_face_edit_distance(h_missing_one_link)
+    assert np.allclose(mfed, 1 / 3)
+
+    mfed = xgi.mean_face_edit_distance(h_missing_one_link, min_size=1)
+    assert np.allclose(mfed, 1 / 6)
+
+    # links and triangles 2
+    mfed = xgi.mean_face_edit_distance(h_links_and_triangles2)
+    assert np.allclose(mfed, 1 / 3)
+
+    mfed = xgi.mean_face_edit_distance(h_links_and_triangles2, min_size=1)
+    assert np.allclose(mfed, 7 / 9)
+
+    mfed = xgi.mean_face_edit_distance(h_links_and_triangles2, exclude_min_size=False)
+    assert np.allclose(mfed, 2 / 9)
 
 
 def test_simplicial_fraction(
