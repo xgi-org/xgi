1e2dcd4ced8e75eb26cbc35fed06e297672dff1b
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-11-05 22:29:49 -0600

    add docstrings and cleanup function

--- xgi/utils/tensor.py
+++ xgi/utils/tensor.py
@@ -84,7 +84,161 @@ def banerjee_coeff(size, max_size):
     )
 
 
-def get_gen_coef_subset_expansion(edge_values, node_value, r):
+def ttsv1(nodedict, edgedict, r, a):
+    """Computes the tensor times same vector in all modes but 1.
+
+    This method uses generating functions as described in the corresponding reference.
+
+    Parameters
+    ----------
+    nodedict : dict
+        A dictionary with nodes as keys and hyperedges they appear in
+        as values.
+    edgedict : dict
+        A dictionary with edges as keys and nodes which are members as
+        values.
+    r : int
+        maximum hyperedge size
+    a : NumPy array
+        the vector to multiply the tensor by.
+
+    Returns
+    -------
+    NumPy array
+        The tensor multiplied by the vector in all modes but 1.
+
+    See Also
+    --------
+    ttsv2
+
+    References
+    ----------
+    Scalable Tensor Methods for Nonuniform Hypergraphs,
+    Sinan Aksoy, Ilya Amburg, Stephen Young,
+    https://doi.org/10.1137/23M1584472
+    """
+    n = len(nodedict)
+    s = np.zeros(n)
+    r_minus_1_factorial = factorial(r - 1)
+    for node, edges in nodedict.items():
+        c = 0
+        for e in edges:
+            l = len(edgedict[e])
+            alpha = banerjee_coeff(l, r)
+            edge_without_node = [v for v in edgedict[e] if v != node]
+            if l == r:
+                gen_fun_coef = prod(a[edge_without_node])
+            elif 2 ** (l - 1) < r * (l - 1):
+                gen_fun_coef = _get_gen_coef_subset_expansion(
+                    a[edge_without_node], a[node], r - 1
+                )
+            else:
+                gen_fun_coef = _get_gen_coef_fft_fast_array(
+                    edge_without_node, a, node, l, r
+                )
+            c += r_minus_1_factorial * l * gen_fun_coef / alpha
+        s[node] = c
+    return s
+
+
+def ttsv2(pairdict, edgedict, r, a, n):
+    """Computes the tensor times same vector in all modes but 2.
+
+    Parameters
+    ----------
+    pairdict : dict
+        A dictionary with node pairs as keys and hyperedges they appear in
+        as values.
+    edgedict : dict
+        A dictionary with edges as keys and nodes which are members as
+        values.
+    r : int
+        maximum hyperedge size
+    a : NumPy array
+        the vector to multiply the tensor by.
+    n : int
+        Number of nodes
+
+    Returns
+    -------
+    Scipy sparse array
+        A 2D array, which is the result of the tensor
+        multiplied by the vector in all modes but 2.
+
+    See Also
+    --------
+    ttsv1
+
+    References
+    ----------
+    Scalable Tensor Methods for Nonuniform Hypergraphs,
+    Sinan Aksoy, Ilya Amburg, Stephen Young,
+    https://doi.org/10.1137/23M1584472
+    """
+    s = {}
+    r_minus_2_factorial = factorial(r - 2)
+    for (v1, v2), edges in pairdict.items():
+        c = 0
+        for e in edges:
+            l = len(edgedict[e])
+            alpha = banerjee_coeff(l, r)
+            edge_without_node = [v for v in edgedict[e] if v != v1 and v != v2]
+            if v1 != v2:
+                if 2 ** (l - 2) < (r - 2) * (l - 2):
+                    gen_fun_coef = _get_gen_coef_subset_expansion(
+                        a[edge_without_node], a[v1] + a[v2], r - 2
+                    )
+                else:
+                    coefs = [1]
+                    for i in range(1, r - 1):
+                        coefs.append(coefs[-1] * (a[v1] + a[v2]) / i)
+                    coefs = np.array(coefs)
+                    for u in edgedict[e]:
+                        if u != v1 and u != v2:
+                            _coefs = [1]
+                            for i in range(1, r - l + 2):
+                                _coefs.append(_coefs[-1] * a[u] / i)
+                            _coefs = np.array(_coefs)
+                            _coefs[0] = 0
+                            coefs = convolve(coefs, _coefs)[0 : r - 1]
+                    gen_fun_coef = coefs[-1]
+            else:
+                if 2 ** (l - 1) < (r - 2) * (l - 1):
+                    gen_fun_coef = _get_gen_coef_subset_expansion(
+                        a[edge_without_node], a[v1], r - 2
+                    )
+                else:
+                    coefs = [1]
+                    for i in range(1, r - 1):
+                        coefs.append(coefs[-1] * (a[v1]) / i)
+                    coefs = np.array(coefs)
+                    for u in edgedict[e]:
+                        if u != v1 and u != v2:
+                            _coefs = [1]
+                            for i in range(1, r - l + 1):
+                                _coefs.append(_coefs[-1] * a[v1] / i)
+                            _coefs = np.array(_coefs)
+                            _coefs[0] = 0
+                            coefs = convolve(coefs, _coefs)[0 : r - 1]
+                    gen_fun_coef = coefs[-1]
+            c += r_minus_2_factorial * l * gen_fun_coef / alpha
+        s[(v1, v2)] = c
+        if v1 == v2:
+            s[(v1, v2)] /= 2
+    first = np.zeros(len(s))
+    second = np.zeros(len(s))
+    value = np.zeros(len(s))
+    for i, k in enumerate(s.keys()):
+        first[i] = k[0]
+        second[i] = k[1]
+        value[i] = s[k]
+    Y = coo_array((value, (first, second)), (n, n))
+    return Y + Y.T
+
+
+## Helper functions for the tensor methods.
+
+def _get_gen_coef_subset_expansion(edge_values, node_value, r):
     k = len(edge_values)
     subset_vector = [0]
     subset_lengths = [0]
@@ -104,17 +258,7 @@ def get_gen_coef_subset_expansion(edge_values, node_value, r):
     return total / factorial(r)
 
 
-def get_gen_coef_fft(edge_without_node, a, node, l, r):
-    coefs = np.array([(a[node] ** i) / factorial(i) for i in range(r)])
-    for u in edge_without_node:
-        _coefs = np.array([a[u] ** i / factorial(i) for i in range(r - l + 2)])
-        _coefs[0] = 0
-        coefs = convolve(coefs, _coefs)[0:r]
-    gen_fun_coef = coefs[-1]
-    return gen_fun_coef
-
-
-def get_gen_coef_fft_fast_array(edge_without_node, a, node, l, r):
+def _get_gen_coef_fft_fast_array(edge_without_node, a, node, l, r):
     coefs = [1]
     for i in range(1, r):
         coefs.append(coefs[-1] * a[node] / i)
@@ -128,92 +272,3 @@ def get_gen_coef_fft_fast_array(edge_without_node, a, node, l, r):
         coefs = convolve(coefs, _coefs)[0:r]
     gen_fun_coef = coefs[-1]
     return gen_fun_coef
-
-
-def ttsv1(E, H, r, a):
-    n = len(E)
-    s = np.zeros(n)
-    r_minus_1_factorial = factorial(r - 1)
-    for node, edges in E.items():
-        c = 0
-        for e in edges:
-            l = len(H[e])
-            alpha = banerjee_coeff(l, r)
-            edge_without_node = [v for v in H[e] if v != node]
-            if l == r:
-                gen_fun_coef = prod(a[edge_without_node])
-            elif 2 ** (l - 1) < r * (l - 1):
-                gen_fun_coef = get_gen_coef_subset_expansion(
-                    a[edge_without_node], a[node], r - 1
-                )
-            else:
-                gen_fun_coef = get_gen_coef_fft_fast_array(
-                    edge_without_node, a, node, l, r
-                )
-            c += r_minus_1_factorial * l * gen_fun_coef / alpha
-        s[node] = c
-    return s
-
-
-def ttsv2(E, H, r, a, n):
-    s = {}
-    r_minus_2_factorial = factorial(r - 2)
-    for nodes, edges in E.items():
-        v1 = nodes[0]
-        v2 = nodes[1]
-        c = 0
-        for e in edges:
-            l = len(H[e])
-            alpha = banerjee_coeff(l, r)
-            edge_without_node = [v for v in H[e] if v != v1 and v != v2]
-            if v1 != v2:
-                if 2 ** (l - 2) < (r - 2) * (l - 2):
-                    gen_fun_coef = get_gen_coef_subset_expansion(
-                        a[edge_without_node], a[v1] + a[v2], r - 2
-                    )
-                else:
-                    coefs = [1]
-                    for i in range(1, r - 1):
-                        coefs.append(coefs[-1] * (a[v1] + a[v2]) / i)
-                    coefs = np.array(coefs)
-                    for u in H[e]:
-                        if u != v1 and u != v2:
-                            _coefs = [1]
-                            for i in range(1, r - l + 2):
-                                _coefs.append(_coefs[-1] * a[u] / i)
-                            _coefs = np.array(_coefs)
-                            _coefs[0] = 0
-                            coefs = convolve(coefs, _coefs)[0 : r - 1]
-                    gen_fun_coef = coefs[-1]
-            else:
-                if 2 ** (l - 1) < (r - 2) * (l - 1):
-                    gen_fun_coef = get_gen_coef_subset_expansion(
-                        a[edge_without_node], a[v1], r - 2
-                    )
-                else:
-                    coefs = [1]
-                    for i in range(1, r - 1):
-                        coefs.append(coefs[-1] * (a[v1]) / i)
-                    coefs = np.array(coefs)
-                    for u in H[e]:
-                        if u != v1 and u != v2:
-                            _coefs = [1]
-                            for i in range(1, r - l + 1):
-                                _coefs.append(_coefs[-1] * a[v1] / i)
-                            _coefs = np.array(_coefs)
-                            _coefs[0] = 0
-                            coefs = convolve(coefs, _coefs)[0 : r - 1]
-                    gen_fun_coef = coefs[-1]
-            c += r_minus_2_factorial * l * gen_fun_coef / alpha
-        s[nodes] = c
-        if v1 == v2:
-            s[nodes] /= 2
-    first = np.zeros(len(s))
-    second = np.zeros(len(s))
-    value = np.zeros(len(s))
-    for i, k in enumerate(s.keys()):
-        first[i] = k[0]
-        second[i] = k[1]
-        value[i] = s[k]
-    Y = coo_array((value, (first, second)), (n, n))
-    return Y + Y.T
