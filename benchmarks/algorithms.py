5bace1ad1d9108a81947f550323737d824209b13
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-11-24 18:06:16 -0500

    Tweak benchmarks (#631)
    * remove unnecessary files
    
    * add benchmarks
    
    * add rounds
    
    * add benchmarks

---
+++ benchmarks/algorithms.py
@@ -0,0 +1,20 @@
+import xgi
+
+rounds = 10
+fname = "benchmarks/email-eu.json"
+
+
+def test_connected(benchmark):
+    def setup():
+        H = xgi.read_hif(fname)
+        return (H,), {}
+
+    benchmark.pedantic(xgi.is_connected, setup=setup, rounds=rounds)
+
+
+def test_clustering_coefficient(benchmark):
+    def setup():
+        H = xgi.read_hif(fname)
+        return (H,), {}
+
+    benchmark.pedantic(xgi.clustering_coefficient, setup=setup, rounds=rounds)
---
+++ benchmarks/generators.py
@@ -0,0 +1,11 @@
+import xgi
+
+rounds = 10
+
+
+def test_erdos_renyi(benchmark):
+
+    def erdos_renyi():
+        xgi.fast_random_hypergraph(100, [0.1, 0.001])
+
+    benchmark.pedantic(erdos_renyi, rounds=rounds)
