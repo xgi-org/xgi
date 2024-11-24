a9c77e41ca65c5e5f1bc8e26e19e6a84604f8fb9
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-11-24 17:12:48 -0500

    fix filepath

--- benchmarks/bench.py
+++ benchmarks/bench.py
@@ -9,7 +9,7 @@ import xgi
 
 def test_construct_from_edgelist(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H.edges.members(),), {}
 
     benchmark.pedantic(xgi.Hypergraph, setup=setup, rounds=10)
@@ -17,7 +17,7 @@ def test_construct_from_edgelist(benchmark):
 
 def test_construct_from_edgedict(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H.edges.members(dtype=dict),), {}
 
     benchmark.pedantic(xgi.Hypergraph, setup=setup, rounds=10)
@@ -25,7 +25,7 @@ def test_construct_from_edgedict(benchmark):
 
 def test_construct_from_df(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (xgi.to_bipartite_pandas_dataframe(H),), {}
 
     benchmark.pedantic(xgi.Hypergraph, setup=setup, rounds=10)
@@ -33,7 +33,7 @@ def test_construct_from_df(benchmark):
 
 def test_node_memberships(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def node_memberships(H):
@@ -44,7 +44,7 @@ def test_node_memberships(benchmark):
 
 def test_edge_members(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def edge_members(H):
@@ -55,7 +55,7 @@ def test_edge_members(benchmark):
 
 def test_node_attributes(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def node_attributes(H):
@@ -66,7 +66,7 @@ def test_node_attributes(benchmark):
 
 def test_edge_attributes(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def edge_attributes(H):
@@ -77,7 +77,7 @@ def test_edge_attributes(benchmark):
 
 def test_degree(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def degree(H):
@@ -88,7 +88,7 @@ def test_degree(benchmark):
 
 def test_nodestats_degree(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def degree(H):
@@ -99,7 +99,7 @@ def test_nodestats_degree(benchmark):
 
 def test_nodestats_degree(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def degree(H):
@@ -110,7 +110,7 @@ def test_nodestats_degree(benchmark):
 
 def test_edge_size(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def degree(H):
@@ -121,7 +121,7 @@ def test_edge_size(benchmark):
 
 def test_isolates(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def isolates(H):
@@ -132,7 +132,7 @@ def test_isolates(benchmark):
 
 def test_singletons(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def singletons(H):
@@ -143,7 +143,7 @@ def test_singletons(benchmark):
 
 def test_copy(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def copy(H):
@@ -154,7 +154,7 @@ def test_copy(benchmark):
 
 def test_dual(benchmark):
     def setup():
-        H = xgi.read_hif("email-enron.json")
+        H = xgi.read_hif("benchmarks/email-enron.json")
         return (H,), {}
 
     def dual(H):
