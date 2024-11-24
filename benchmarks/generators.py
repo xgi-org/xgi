323513c98b1baede8e42e6170c22632050b9bf3d
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-11-24 18:05:52 -0500

    add benchmarks

--- benchmarks/generators.py
+++ benchmarks/generators.py
@@ -4,7 +4,8 @@ rounds = 10
 
 
 def test_erdos_renyi(benchmark):
-    def erdos_renyi(H):
+
+    def erdos_renyi():
         xgi.fast_random_hypergraph(100, [0.1, 0.001])
 
     benchmark.pedantic(erdos_renyi, rounds=rounds)
