71b897f9395dea85bac449f81aaec866694ae1b0
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-11-24 18:42:53 -0500

    fix iterations

--- benchmarks/generators.py
+++ benchmarks/generators.py
@@ -1,6 +1,7 @@
 import xgi
 
 rounds = 10
+iterations = 1
 
 
 def test_erdos_renyi(benchmark):
@@ -8,4 +9,4 @@ def test_erdos_renyi(benchmark):
     def erdos_renyi():
         xgi.fast_random_hypergraph(100, [0.1, 0.001])
 
-    benchmark.pedantic(erdos_renyi, rounds=rounds)
+    benchmark.pedantic(erdos_renyi, rounds=rounds, iterations=1)
