7ffb660ecce4c2fb50e122cc2b7cf209c8975d65
Maxime Lucas <maxime.lucas.work@gmail.com>
2025-02-05 22:16:51 +0100

    updated example lw

--- examples/basic/plot_simple_hypergraph_outline.py
+++ examples/basic/plot_simple_hypergraph_outline.py
@@ -15,6 +15,6 @@ H = xgi.Hypergraph(hyperedges)
 
 
 pos = xgi.barycenter_spring_layout(H, seed=1)
-xgi.draw(H, pos=pos, hull=True, edge_fc="white", edge_lw=15)
+xgi.draw(H, pos=pos, hull=True, edge_fc="white", edge_lw=1)
 
 plt.show()
