2f323ffd165a3cb43d800d6cb7330b7bff7466aa
Daniel Kaiser <dkaiser4897@gmail.com>
2025-02-16 18:37:37 -0500

    feat(kmeans): implement kmeans

--- xgi/communities/spectral.py
+++ xgi/communities/spectral.py
@@ -10,6 +10,8 @@ __all__ = [
     "spectral_clustering",
 ]
 
+MAX_ITERATIONS = 10_000
+
 
 def spectral_clustering(H, k=None):
     if k is None:
@@ -35,6 +37,44 @@ def spectral_clustering(H, k=None):
 
 
 def _kmeans(X, k):
+    # Handle edge cases
     if k == 1:
         return {node_idx: 1 for node_idx in range(X.shape[0])}
-    pass
+
+    # Initialize stopping criterion
+    num_cluster_changes = np.inf
+    num_iterations = 0
+
+    # Instantiate random centers
+    bounds_inf = X.min(axis=0)
+    bounds_sup = X.max(axis=0)
+    width = bounds_sup - bounds_inf
+
+    # NOTE: Want Hadamard product here
+    centroids = width * np.random.random((k, X.shape[1]))
+
+    # Instantiate random clusters
+    previous_clusters = {node: np.random.randint(0, k) for node in range(X.shape[0])}
+
+    # Iterate main kmeans computation
+    while (num_cluster_changes > 0) and (num_iterations < MAX_ITERATIONS):
+        # Find nearest centroid to each point
+        next_clusters = dict()
+        for node, vector in enumerate(X):
+            distances = list(
+                map(lambda centroid: np.linalg.norm(vector - centroid), centroids)
+            )
+            closest_centroid = np.argmin(distances)
+            next_clusters[node] = closest_centroid
+
+        # Update convergence condition
+        cluster_changes = {
+            node: next_clusters[node] != previous_clusters[node]
+            for node in range(X.shape[0])
+        }
+        num_cluster_changes = len(
+            list(filter(lambda diff: diff, cluster_changes.values()))
+        )
+        num_iterations += 1
+
+    return next_clusters
