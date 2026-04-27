bd084900a4c9228aa05dad1c076ab9024b1aa976
Maxime Lucas <maxime.lucas.work@gmail.com>
2026-04-27 10:58:24 +0200

    added star clique and RSC to gallery

---
+++ examples/generative_models/plot_random_simplicial_complex.py
@@ -0,0 +1,25 @@
+"""
+=========================
+Random simplicial complex
+=========================
+
+Generate a random hypergraph.
+"""
+
+import matplotlib.pyplot as plt
+
+import xgi
+
+# specify parameters
+seed = 1 # fix random number generation
+n = 20 # number of nodes
+ps = [0.15, 0.01] # wiring probabilities
+
+# generate hypergraph
+H = xgi.random_simplicial_complex(n, ps, seed=seed)
+
+# draw hypergraph
+pos = xgi.barycenter_spring_layout(H, seed=seed)
+xgi.draw(H, pos=pos)
+
+plt.show()
