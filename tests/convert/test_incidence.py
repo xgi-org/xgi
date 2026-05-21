9a8840ecefa6233ef26b14eb8e68d3143cd83e2b
Maxime Lucas <maxime.lucas.work@gmail.com>
2026-05-21 16:08:36 +0200

    fix: issue #718

--- tests/convert/test_incidence.py
+++ tests/convert/test_incidence.py
@@ -11,3 +11,16 @@ def test_to_incidence_matrix(edgelist5, incidence5):
 def test_from_incidence_matrix(edgelist5, incidence5):
     H = xgi.from_incidence_matrix(incidence5)
     assert H.edges.members() == edgelist5
+
+
+def test_fix_718():
+    # Adding an edge after from_incidence_matrix must not collide with existing edge IDs
+    B = np.array([[1, 1, 0], [1, 0, 1], [0, 1, 1]])
+    H = xgi.from_incidence_matrix(B)
+    H.add_edge([0, 3])
+    assert H.edges.members(dtype=dict) == {
+        0: {0, 1},
+        1: {0, 2},
+        2: {1, 2},
+        3: {0, 3},
+    }
